ac2a5b954741c9adf2b82010977e26d2e2eb4448
Maria Reina <mariareinacookg@gmail.com>
2022-07-04 09:53:49 +0200

    ingreso bd

--- src/main/java/com/example/personajes/personajes.java
+++ src/main/java/com/example/personajes/personajes.java
@@ -1,5 +1,16 @@
 package com.example.personajes;
 
+import javax.persistence.Entity;
+
+@Entity
+
 public class personajes {
 
+	private Integer id;
+	private String name;
+	private String status;
+	private String gender;
+	private String image;
+	
+	
 }
