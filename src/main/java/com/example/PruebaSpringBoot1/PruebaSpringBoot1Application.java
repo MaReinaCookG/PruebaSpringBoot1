90d4c857adc74166c4f9ba7d447596e976f86cac
Maria Reina <mariareinacookg@gmail.com>
2022-07-05 09:28:30 +0200

    actualizar

---
+++ src/main/java/com/example/PruebaSpringBoot1/PruebaSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example.PruebaSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringBoot1Application.class, args);
+	}
+
+}
